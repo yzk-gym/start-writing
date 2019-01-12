06647aa48e9f6dc59f79cde9d4c1118e1ec7a093
yuya <siren.night43@gmail.com>
2019-01-12 11:35:41 +0900

    一旦コミット

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD1yGsxATyeJ-LlwgtAgFZSk3juieScJys",
+  authDomain: "first-write.firebaseapp.com",
+  databaseURL: "https://first-write.firebaseio.com",
+  projectId: "first-write",
+  storageBucket: "first-write.appspot.com",
+  messagingSenderId: "1090385959667"
+};
+firebase.initializeApp(config);
