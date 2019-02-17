1830a1a1b13fdcf11a00d6e90ac0f9dda5c730e9
yuya <siren.night43@gmail.com>
2019-02-17 16:07:47 +0900

    テスト修正

--- functions/ogp.js
+++ functions/ogp.js
@@ -1,5 +1,7 @@
 exports.handler = function(event, context, callback) {
   console.log('Hello Netlify Functions.');
+  console.log("ログ");
+  console.log(event)
   console.log(context);
   callback(null, {
     statusCode: 200,
