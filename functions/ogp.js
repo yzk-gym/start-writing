6756d88864d7120ee552e1764576c684bd34f6c5
yuya <siren.night43@gmail.com>
2019-02-19 23:54:42 +0900

    テストa

--- functions/ogp.js
+++ functions/ogp.js
@@ -3,6 +3,7 @@ exports.handler = function(event, context, callback) {
   console.log("ログ");
   console.log(event)
   console.log(context);
+  console.log(event.queryStringParameters.path);
   callback(null, {
     statusCode: 200,
     body: "テスト",
