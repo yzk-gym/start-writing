41550b387e9ee0bbefd1746ec810abf03c93f02e
yuya <siren.night43@gmail.com>
2019-02-28 23:51:15 +0900

    テスト

--- functions/ogp.js
+++ functions/ogp.js
@@ -1,12 +1,10 @@
 exports.handler = function(event, context, callback) {
-  console.log('Hello Netlify Functions.');
-  console.log("ログ");
-  console.log(event)
-  console.log(context);
-  console.log("テスト")
-  console.log(event.queryStringParameters);
   callback(null, {
     statusCode: 200,
-    body: `<meta http-equiv="refresh" content="0; URL='https://yzkamp.netlify.com'" />`,
+    body: `<html>
+<head>
+<meta http-equiv="refresh" content="0; URL='http://new-website.com'" />
+</head>
+</html>`,
   });
 }
