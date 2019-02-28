d6b3567ef18f7b07648a6cf8a327e917b21a73e5
yuya <siren.night43@gmail.com>
2019-03-01 00:41:58 +0900

    不要な処理の削除

--- functions/ogp.js
+++ functions/ogp.js
@@ -1,12 +1,12 @@
 exports.handler = function(event, context, callback) {
   console.log("テスト")
-  console.log(context)
   console.log(event)
+  const EVENT_ID = event.queryStringParameters.path
   callback(null, {
     statusCode: 200,
     body: `<html>
 <head>
-<meta http-equiv="refresh" content="0; URL='https://yzkamp.netlify.com/events/1'" />
+<meta http-equiv="refresh" content="0; URL='https://yzkamp.netlify.com/events/${EVENT_ID}'" />
 </head>
 </html>`,
   });
