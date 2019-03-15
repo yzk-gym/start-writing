96d85932dede38a2dda3bff0079fda2d8313b3f4
yuya <siren.night43@gmail.com>
2019-03-15 20:27:29 +0900

    test

--- functions/ogp.js
+++ functions/ogp.js
@@ -1,6 +1,16 @@
-const firebase = require('./js/firebase');
+
+
 exports.handler = function(event, context, callback) {
-  console.log("test")
+  const firebase =  require('firebase');
+  var config = {
+    apiKey: "AIzaSyD1yGsxATyeJ-LlwgtAgFZSk3juieScJys",
+    authDomain: "first-write.firebaseapp.com",
+    databaseURL: "https://first-write.firebaseio.com",
+    projectId: "first-write",
+    storageBucket: "first-write.appspot.com",
+    messagingSenderId: "1090385959667"
+  };
+  firebase.initializeApp(config);
   console.log(firebase)
   const EVENT_ID = event.queryStringParameters.path
   callback(null, {
